0579f187de128f230603baee3e3d51f153955921
nils-vanderwerf <n.vanderw.92@gmail.com>
2021-08-03 09:55:14 +1000

    Added redux folder

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware} from 'redux' 
+import rootReducer from './redux/rootReducer.js'
 import './index.css';
 import App from './App';
 
